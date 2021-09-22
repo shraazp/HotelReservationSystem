ab15adda052aa53f3da42429ce275bd7bee2c379
shraazp <shravyap99@gmail.com>
2021-09-22 13:21:19 +0530

    refactored:cheapest hotel is selected

--- test/java/com/user/hotel/reservation/system/HotelReservationSystemTest.java
+++ test/java/com/user/hotel/reservation/system/HotelReservationSystemTest.java
@@ -1,17 +1,22 @@
 package com.user.hotel.reservation.system;
 import org.junit.Test;
-import java.util.Date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import static org.junit.Assert.assertEquals;
 public class HotelReservationSystemTest {
     @Test
-    public void CheckIfHotelIsAdded()
+    public void CheckIfHotelIsAdded() throws ParseException
     {
         HotelReservationSystem hotelSystem=new HotelReservationSystem();
         hotelSystem.addHotel("Lakewood",110);
         hotelSystem.addHotel("Ridgewood",220);
         hotelSystem.addHotel("Bridgewood",160);
         int entry=hotelSystem.countHotels();
-        String entryHotel=hotelSystem.cheapHotel(new Date(2000,12,18),new Date(2000,12,28));
+        SimpleDateFormat formatter = new SimpleDateFormat("ddMMMyyyy", Locale.ENGLISH);
+        String entryHotel=hotelSystem.cheapHotel(formatter.parse("10Sep2020"),formatter.parse("19Sep2020"));
         assertEquals(3,entry);
         assertEquals("Lakewood",entryHotel);
     }
